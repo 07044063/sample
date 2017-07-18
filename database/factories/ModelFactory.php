6f634613dfffc69fc535a1beaddbe2d402db9f2a
07044063 <07044063@163.com>
2017-07-18 17:02:40 +0800

    finish user vacation

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,6 +21,7 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
         'is_admin' => false,
         'email' => $faker->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
+        'activated' => true,
         'remember_token' => str_random(10),
         'created_at' => $date_time,
         'updated_at' => $date_time,
